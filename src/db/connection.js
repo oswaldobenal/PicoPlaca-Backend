9bc0779175caf93d32fff9a820978508a1d49ae9
Julian <oswaldobenal12@gmail.com>
2022-08-18 18:48:02 -0500

    SSl config

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 try {
-  await mongoose.connect(process.env.URI_MONGO);
+  await mongoose.connect(process.env.URI_MONGO, { ssl: true });
   console.log("Connect DB ok 👋");
 } catch (error) {
   console.log("Error de conexión a mongodb:" + error);
